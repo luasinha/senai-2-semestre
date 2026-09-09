a3c798c8ddd8fcc9826eaf39ad1d3a05482a15a3
luasinha <luannamuller1981@gmail.com>
2026-09-09 20:49:00 -0300

    aa

---
+++ exercicios js 26.08/exercicio12.js
@@ -0,0 +1,11 @@
+function exercicio12(){
+
+let agora = new Date()
+
+
+
+console.log(`Estamos no ano ${agora.getFullYear}`)
+
+}
+
+export default exercicio12;
---
+++ exercicios js 26.08/exercicio14.js
@@ -0,0 +1,13 @@
+import numeros from "./variaveis";
+
+function exercicio14(){
+
+for(let i = 0; i < numeros.length; i++){
+    if(numeros[i] > 20){
+        console.log(numeros[i])
+    }
+}
+
+}
+
+export default exercicio14;
