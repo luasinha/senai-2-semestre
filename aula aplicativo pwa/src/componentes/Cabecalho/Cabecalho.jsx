d8194b4a94771a895d61bb33b87db25d48800a3f
luasinha <luannamuller1981@gmail.com>
2026-09-16 19:27:46 -0300

    asdasd

---
+++ aula aplicativo pwa/src/componentes/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,10 @@
+import './Cabecalho.css'
+
+function Cabecalho() {
+    return <header className='Cabecalho_root'>
+        <img src='/favicon.svg' />
+        <h4>AVATAR</h4>
+    </header>
+}
+
+export default Cabecalho
